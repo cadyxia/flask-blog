59ae8b343a2fea4e042936dab1b408e03e0a60e5
cady <cadyxia@gmail.com>
2025-09-13 19:03:54 -0700

    implemented detailed view and comment functionalities

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -68,7 +68,7 @@ def update(id):
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
-  get_post(id)
+  # get_post(id)
   db = get_db()
   db.execute('DELETE FROM post WHERE id = ?', (id,))
   db.commit()
@@ -84,3 +84,33 @@ def search():
                      (query,)).fetchall()
   g.query = query
   return render_template('blog/index.html', posts=posts)
+
+def get_comments(id):
+  comments = get_db().execute('SELECT body, created, username'
+                     ' FROM comments c JOIN user u ON c.author_id = u.id'
+                     ' WHERE post_id = ? ORDER BY created DESC',
+                     (id,)).fetchall()
+  # comments = get_db().execute('SELECT author_id, body, created'
+  #                         ' FROM comments WHERE post_id = ?', (id,)).fetchall()
+  return comments
+
+@bp.route('/<int:id>/detailed-post', methods=('GET',))
+def detailed_post(id):
+  post = get_post(id, False)
+  comments = get_comments(id)
+  return render_template('blog/detailed-post.html', post=post, comments=comments)
+
+@bp.route('/comment', methods=('POST',))
+@login_required
+def comment():
+  post_id = request.form['post_id']
+  author_id = request.form['author_id']
+  body = request.form['body']
+  
+  db = get_db()
+  db.execute('INSERT INTO comments (post_id, author_id, body) VALUES (?, ?, ?)',
+             (post_id, author_id, body))
+  db.commit()
+  comments=get_comments(post_id)
+  flash("Comment posted!")
+  return redirect(url_for('blog.detailed_post', id=post_id, comments=comments))
