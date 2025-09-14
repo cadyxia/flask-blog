10842b3727d972c45528bfd3d9306bb02a7d32b5
cady <cadyxia@gmail.com>
2025-09-13 17:50:43 -0700

    implemented basic search feature

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -73,3 +73,14 @@ def delete(id):
   db.execute('DELETE FROM post WHERE id = ?', (id,))
   db.commit()
   return redirect(url_for('blog.index'))
+
+@bp.route('/search', methods=('POST',))
+def search():
+  query = request.form['query']
+  db = get_db()
+  posts = db.execute("SELECT p.id, title, body, created, author_id, username "
+                     "FROM post p JOIN user u ON p.author_id = u.id "
+                     "WHERE body LIKE '%'||?||'%'  ORDER BY created DESC",
+                     (query,)).fetchall()
+  g.query = query
+  return render_template('blog/index.html', posts=posts)
